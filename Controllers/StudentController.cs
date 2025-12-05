9729bf60e3738fb9f187af2516b3fd274b6638b5
Hadi Nasehi <hadi.nasehi@gmail.com>
2025-12-05 17:24:34 +0330

    Add student controller

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UniversityManagement.Models;
+
+namespace UniversityManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new Student
+            {
+                FullName="Mahan",
+                Id=1
+            })
+            .ToArray();
+        }
+    }
+}
