a3a3be479176ac434f9f0e04c5adab891733ca7d
Mahannasehi <nasehimahan@gmail.com>
2026-01-01 02:00:15 +0330

    Refactor: extract EnrollRequest into DTO layer

--- Controllers/EnrollmentController.cs
+++ Controllers/EnrollmentController.cs
@@ -89,10 +89,4 @@ namespace UniversityManagement.Controllers
             });
         }
     }
-
-    public class EnrollRequest
-    {
-        public int StudentId { get; set; }
-        public List<int> LessonIds { get; set; } = new();
-    }
 }
